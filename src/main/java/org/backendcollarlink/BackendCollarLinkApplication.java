5706d2c2c67923234640bc85f87ee7559d629c31
JCarbajal22 <u202121881@upc.edu.pe>
2025-06-08 10:56:47 -0500

    .

---
+++ src/main/java/org/backendcollarlink/BackendCollarLinkApplication.java
@@ -0,0 +1,13 @@
+package org.backendcollarlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCollarLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCollarLinkApplication.class, args);
+    }
+
+}
